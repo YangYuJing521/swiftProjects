88692eceb20ca43dc9a2f5a28479c3825984bfcc
silverBullet <ios_yangyujing@163.com>
2019-10-06 16:22:00 +0800

    project 02 init

---
+++ project-02 stopWatch/project-02 stopWatch/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  project-02 stopWatch
+//
+//  Created by 杨玉京 on 2019/10/6.
+//  Copyright © 2019 杨玉京. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: -UI Components
+    @IBOutlet weak var SubsidiaryClock: UILabel!
+    @IBOutlet weak var MainClockLabel: UILabel!
+    @IBOutlet weak var lapButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var mainTable: UITableView!
+    
+    // MARK: -Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
