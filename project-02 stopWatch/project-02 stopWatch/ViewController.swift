ac7bfb6ca2110c883cb0b1d1e100f20995f6a011
silverBullet <ios_yangyujing@163.com>
2019-10-06 18:47:11 +0800

    basic UI and Actions

--- project-02 stopWatch/project-02 stopWatch/ViewController.swift
+++ project-02 stopWatch/project-02 stopWatch/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: -Variables
+    fileprivate var isPlay: Bool = false
+    fileprivate var laps: [String] = []
+//    fileprivate var laps: [String]?
 
     // MARK: -UI Components
     @IBOutlet weak var SubsidiaryClock: UILabel!
@@ -20,9 +24,49 @@ class ViewController: UIViewController {
     // MARK: -Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //给button设置一些属性
+        let initCircleButton: (UIButton) -> Void = { button in
+            button.layer.cornerRadius = 0.5*button.bounds.size.width
+            button.backgroundColor = UIColor.white
+        }
+        initCircleButton(lapButton)
+        initCircleButton(startButton)
+        
+        mainTable.dataSource = self;
     }
 
-
+    // MARK: -UI seeting
+    override var shouldAutorotate: Bool{
+        return false
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return UIStatusBarStyle.lightContent
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask{
+        return UIInterfaceOrientationMask.portrait
+    }
+    
+    // MARK: -Actions
+    @IBAction func lapButtonTouchd(_ sender: Any) {
+    }
+    @IBAction func startButtonTouched(_ sender: Any) {
+    }
 }
 
+
+// MARK: -Tableview Datasource
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return laps.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier: String = "clockCellId"
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        return cell
+    }
+
+}
